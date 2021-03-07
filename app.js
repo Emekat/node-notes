0b7403ab6a890438b0643971cd8c835f74b06525
emekat <emekatorti@gmail.com>
2021-03-07 17:27:03 -0500

    added pug templating engine and added sample temp

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 //jshint esversion:6
 
 const express = require('express');
-const greetMiddleware = require('./greet.js');
-
-
-class GreetingService {
-    constructor(greeting = 'Hello') {
-    this.greeting = greeting;
-    }
-       createGreeting(name) {
-       return `${this.greeting}, ${name}!`;
-    }
-}
-
 const app = express();
 
-app
-  .use('/api/v1/service1', greetMiddleware({ 
-    service: new GreetingService('Hello')}))
-  .use('/api/v1/service2', greetMiddleware({ 
-        service: new GreetingService('Hi')}))
-    .listen(8080);
+const PORT =  3000;
 
+app.set('view engine', 'pug');
+app.set('views', 'src/views');
+
+//Creates a Root Route
+app.get('/',function(req, res){
+    res.render('index', {name: "Emeka's"}); //renders the index.jade file into html and returns as a response. The
+    //render function also optionally takes the data to pass to the view.
+    });
+app.listen(PORT, (err) => {
+        if (!err) {
+           console.log('Server is running at port', PORT);} 
+        else {
+            console.log(JSON.stringify(err));
+             }
+        });
